ef95af2627e9ee44622b8e8c5c986ef59b6e4cdd
ummeed23101 <142231547+ummeed23101@users.noreply.github.com>
2025-01-30 15:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (35.40%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int helperClimb(int n, vector<int> &dp){
-        if(n==0) return 0;
+        if(n==0) return 1;
         if(n==1) return 1;
-        if(n==2) return 2;
 
         if(dp[n]!=-1) return dp[n];
 
