35d8d8d213d58a421b6602dc72105b87656b0458
ummeed23101 <142231547+ummeed23101@users.noreply.github.com>
2025-03-02 14:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (95.73%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    void helper(int start, int k, int target, vector<int> &levels, vector<vector<int>> &answer) {
+   void helper(int currentIndex, int target, int k, vector<int> &levels, vector<vector<int>> &answer, vector<int> &nums) {
+        if (target < 0 || levels.size() > k) return;  
+
         if (target == 0 && levels.size() == k) {
             answer.push_back(levels);
             return;
         }
 
-        for (int i = start; i <= 9; i++) {
-            if (target < i) break;  
+        if (currentIndex == nums.size()) return; 
 
-            levels.push_back(i);
-            helper(i + 1, k, target - i, levels, answer);
-            levels.pop_back(); 
-        }
+        levels.push_back(nums[currentIndex]);
+        helper(currentIndex + 1, target - nums[currentIndex], k, levels, answer, nums);
+        levels.pop_back();
+        helper(currentIndex + 1, target, k, levels, answer, nums);
     }
 
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>> answer;
+        vector<int> nums = {1,2,3,4,5,6,7,8,9};
         vector<int> levels;
-        helper(1, k, n, levels, answer);
+        vector<vector<int>> answer;
+
+        helper(0, n, k, levels, answer, nums);
         return answer;
     }
 };
