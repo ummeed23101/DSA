95e52afaa554af1e86e3be4ae572f8824bc44699
Ummeed <142231547+ummeed23101@users.noreply.github.com>
2025-03-23 10:53:35 +0530

    Time: 108 ms (5.02%) | Memory: 41.2 MB (5.00%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,40 +1,36 @@
 class Solution {
 public:
 
-   void helper(int currentIndex, vector<int> &candidates, int n, int sum, vector<int> levels, vector<vector<int>> &result){
-        if(sum==0){
+   void helperCombination2(vector<int>& candidates, int target, int n, int currentIndex, vector<int> levels, vector<vector<int>> &result){
+
+        if(target==0){
             result.push_back(levels);
             return;
         }
-        if(currentIndex == n || sum<0){
-            return;
-        }
+        //base case
+        if(currentIndex == n || target<0) return;
 
+        
         levels.push_back(candidates[currentIndex]);
-        helper(currentIndex+1, candidates, n, sum - candidates[currentIndex], levels, result);
+        helperCombination2(candidates, target- candidates[currentIndex], n, currentIndex+1, levels, result);
         levels.pop_back();
 
         for(int i=currentIndex+1; i<n; i++){
-            if(candidates[i]!=candidates[currentIndex]){
-                helper(i,candidates, n, sum, levels, result);
+            if(candidates[currentIndex]!= candidates[i]){
+                helperCombination2(candidates, target, n , i, levels, result);
                 break;
             }
         }
 
-
     }
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        int n = candidates.size();
-        int currentIndex = 0;
-        sort(candidates.begin(), candidates.end());
-        int sum = target;
         vector<int> levels;
         vector<vector<int>> result;
-        helper(currentIndex, candidates, n, sum, levels, result);
+        sort(candidates.begin(), candidates.end());
+        int n = candidates.size();
+        helperCombination2(candidates, target, n, 0, levels, result);
 
         return result;
-
-
     }
 };
-
