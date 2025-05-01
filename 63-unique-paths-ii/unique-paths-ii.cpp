024c7175d7fe655a78f97fccc95375d4cecc6313
Ummeed <142231547+ummeed23101@users.noreply.github.com>
2025-05-01 15:41:28 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (28.46%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
 
-    int helperUniquePath(vector<vector<int>>& obstacleGrid, int m, int n, vector<vector<int>> &dp){     
-        if(m<0 || n<0) return 0;
-        if( obstacleGrid[0][0]==1) return 0;
-        if(m==0 && n==0) return 1;
+    int helper(vector<vector<int>> &matrix, vector<vector<int>> &dp, int m, int n, int currRow, int currCol){
+        if(currRow<0 || currCol<0) return 0;
 
-        if(obstacleGrid[m][n]==1) return 0;
-        
+        if(currRow==0 && currCol==0){
+            return 1;
+        }
 
-        if(dp[m][n]!=-1) return dp[m][n];
+        if(dp[currRow][currCol]!=-1) return dp[currRow][currCol];
+        int up=0; int left = 0;
+        if(currRow-1 >=0 && matrix[currRow-1][currCol]==0) up = helper(matrix, dp, m, n, currRow-1, currCol);
+        if(currCol-1 >=0 && matrix[currRow][currCol-1]==0) left = helper(matrix, dp , m , n ,currRow, currCol-1);
 
+        return dp[currRow][currCol] = left + up;
 
-        int up = helperUniquePath(obstacleGrid, m-1, n, dp);
-        int left = helperUniquePath(obstacleGrid, m, n-1, dp);
-
-        return dp[m][n]= up+left;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        
 
+
+
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n,-1));
+        vector<vector<int>> dp (m, vector<int>(n,-1));
+        if(obstacleGrid[m-1][n-1]==1) return 0;
+        int ans = helper(obstacleGrid, dp, m, n, m-1, n-1);
 
-        return helperUniquePath(obstacleGrid, m-1,n-1, dp);
+        return ans;
     }
 };
